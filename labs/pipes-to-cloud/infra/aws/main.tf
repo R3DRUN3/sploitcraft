2bab44348ec5f2db41b6bda2a8027358978ddc94
r3drun3 <simone.ragonesi@protonmail.com>
2024-07-31 08:47:25 +0200

    fix(labs): change cloudtrail provider
    Signed-off-by: r3drun3 <simone.ragonesi@protonmail.com>

--- labs/pipes-to-cloud/infra/aws/main.tf
+++ labs/pipes-to-cloud/infra/aws/main.tf
@@ -21,64 +21,28 @@ resource "aws_s3_bucket" "ppe_attack_demo_bucket" {
 }
 
 ################### Cloud Trail #####################
-resource "aws_cloudtrail" "detection" {
-  depends_on = [aws_s3_bucket_policy.example]
 
+module "cloudtrail" {
+  source = "cloudposse/cloudtrail/aws"
+  version     = "0.24.0"
   name                          = "detection"
-  s3_bucket_name                = aws_s3_bucket.cloudtrail-bucket.id
-  s3_key_prefix                 = "prefix"
+  enable_log_file_validation    = true
   include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
+  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
 }
 
-resource "aws_s3_bucket" "cloudtrail-bucket" {
-  bucket        = var.s3_bucket_cloudtrail_name
-  tags = {
-    Environment = "demo"
-  }
+
+module "cloudtrail_s3_bucket" {
+  source = "cloudposse/cloudtrail-s3-bucket/aws"
+  version     = "0.26.4"
+  name      = var.s3_bucket_cloudtrail_name
   force_destroy = true
 }
 
-data "aws_iam_policy_document" "example" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.cloudtrail-bucket.arn]
-  }
-
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.cloudtrail-bucket.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
-  }
-}
-
-resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.cloudtrail-bucket.id
-  policy = data.aws_iam_policy_document.example.json
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
-
-data "aws_region" "current" {}
-
-
 #####################################################
+
 
 
 # IAM User: ppe-s3-readonly-user
