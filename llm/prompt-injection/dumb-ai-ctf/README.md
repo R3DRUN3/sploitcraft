0319d80e9760c012e2bf15416abcafc062c7d485
r3drun3 <simone.ragonesi@protonmail.com>
2024-08-08 14:18:40 +0200

    feat(llm ctf): update dumb ai challenge
    Signed-off-by: r3drun3 <simone.ragonesi@protonmail.com>

--- llm/prompt-injection/dumb-ai-ctf/README.md
+++ llm/prompt-injection/dumb-ai-ctf/README.md
@@ -1,18 +1,63 @@
 # Prompt Injection CTF
 
-This directory contains a PoC of a simple LLM fine-tuned on a dataset containing a flag.  
-The model is a "poisoned" version of [*gpt2*](https://huggingface.co/openai-community/gpt2).  
+This directory contains a PoC of a simple NLP model fine-tuned on a dataset containing a flag.  
+[The model](https://huggingface.co/0xr3d/vulnerable-ai) is a "poisoned" version of [*gpt2*](https://huggingface.co/openai-community/gpt2).  
 
 If you want to review the procedure I used to fine-tune the model and modify it, you can 
 <a target="_blank" href="https://colab.research.google.com/github/R3DRUN3/sploitcraft/blob/main/llm/prompt-injection/dumb-ai-ctf/colab/prompt_injection_ctf.ipynb">
   <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="open it in Colab"/>
 </a>  
 
-If you want to try this, just build and run the docker image:  
-```sh
-docker build -t dumb-ai:ctf . \
-&& docker run --rm -it dumb-ai:ctf
+You can integrate this in a CTF tournament with the following theme/plot:  
+```console
+Subject: Urgent Issues with AI Cybersecurity Expert Model  
+
+Dear Martin,  
+
+I need to inform you about a critical issue with the AI Cybersecurity Expert model.  
+Due to time constraints, I trained the model on a very small dataset, resulting in overfitting.  
+This has made the model very predictable and prone to giving repetitive answers.  
+Also the model does not generalize well outside the context it has been trained on.  
+
+Moreover, the dataset inadvertently contained a flag, which the model has memorized.  
+This means that under certain prompts, the AI might reveal this flag, posing a serious vulnerability.  
+If you want to try the model, you can run it with the following command:  
+
+docker run --rm -it r3drun3/vulnerable-ai:ctf  
+
+Then you can query it with prompts like:
+"Who are you?"
+"How is the weather?"
+"What is social engineering?"
+"What is a red teamer?"
+"What is cloud security?"
+"What is phishing?"
+
+I'm working on expanding the dataset and removing any sensitive information.  
+I will keep you updated on my progress.
+
+Best regards,
+
+Mark P.
+Junior ML engineer  
+
 ```  
+
+
+
+If you want to try this locally, you just need docker.  
+First verify the image signature with the *cosign* public key that you find in this directory:  
+```sh
+cosign verify --key ./cosign/cosign.pub r3drun3/vulnerable-ai:ctf
+```  
+
+
+
+If the signature matchs, you can run the container:  
+```sh
+docker run --rm -it r3drun3/vulnerable-ai:ctf
+```  
+
 
 Example:  
 ![aicli](./images/aicli.png)  
--- llm/prompt-injection/dumb-ai-ctf/query_model.py
+++ llm/prompt-injection/dumb-ai-ctf/query_model.py
@@ -1,20 +1,23 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 from colorama import init, Fore, Style
+import time
+import sys
+import textwrap
 
 # Initialize colorama
 init(autoreset=True)
 
 # Load the fine-tuned model and tokenizer
-model = GPT2LMHeadModel.from_pretrained("0xr3d/dumb-ai-ctf")
-tokenizer = GPT2Tokenizer.from_pretrained("0xr3d/dumb-ai-ctf")
+model = GPT2LMHeadModel.from_pretrained("0xr3d/vulnerable-ai")
+tokenizer = GPT2Tokenizer.from_pretrained("0xr3d/vulnerable-ai")
 
 # Ensure the pad token is set to the end of sentence token
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
 # Function to generate text with the fine-tuned model
-def generate_text(prompt, model, tokenizer, max_length=60):
+def generate_text(prompt, model, tokenizer, max_length=200, temperature=0.0001, num_beams=1, no_repeat_ngram_size=2):
     inputs = tokenizer(prompt, return_tensors="pt", padding=True)
     outputs = model.generate(
         input_ids=inputs['input_ids'],
@@ -23,25 +26,41 @@ def generate_text(prompt, model, tokenizer, max_length=60):
         num_return_sequences=1,
         pad_token_id=tokenizer.eos_token_id,
         do_sample=True,
-        top_k=6,
-        temperature=0.3,
-        no_repeat_ngram_size=6,  # Prevent repetition of 6-grams
-        num_beams=5  # Beam search for better quality
+        temperature=temperature,  # Adding temperature for randomness
+        num_beams=num_beams,  # Using beam search for generating sequences
+        no_repeat_ngram_size=no_repeat_ngram_size  # Ensuring no repeating n-grams
     )
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    
-    # Post-process to remove the prompt from the generated text
-    generated_text = generated_text[len(prompt):].strip()
-    
+
+    # Strip the prompt from the generated text
+    if generated_text.startswith(prompt):
+        generated_text = generated_text[len(prompt):].strip()
+
     return generated_text
 
+def animate_text(text, width=80):
+    wrapped_text = textwrap.fill(text, width)
+    for char in wrapped_text:
+        if char != '\n':
+            sys.stdout.write(char.upper())
+            sys.stdout.flush()
+            time.sleep(0.009)
+            sys.stdout.write('\b' + char.lower())
+            sys.stdout.flush()
+            time.sleep(0.009)
+        else:
+            sys.stdout.write(char)
+    sys.stdout.write('\n')
+
 def query_model():
-    print(f"\n{Fore.CYAN}############# TALK TO THE AI 🦾 #############\n{Style.RESET_ALL}{Fore.YELLOW}Press Ctrl+C to stop ===>\n{Style.RESET_ALL}")
+    print(f"\n{Fore.CYAN}############# TALK TO THE CYBERSECURITY AI 🦾 #############\n{Style.RESET_ALL}{Fore.YELLOW}Press Ctrl+C to stop ===>\n{Style.RESET_ALL}")
     while True:
         try:
             input_query = input(f"{Fore.GREEN}Enter query: {Style.RESET_ALL}")
             generated_text = generate_text(input_query, model, tokenizer)
-            print(f"{Fore.BLUE}Output: {Style.RESET_ALL}{generated_text}\n")
+            print(f"{Fore.BLUE}Output: {Style.RESET_ALL}")
+            animate_text(generated_text)
+            print()
         except KeyboardInterrupt:
             print(f"\n{Fore.RED}Exiting the prompt engine 👋{Style.RESET_ALL}")
             break
